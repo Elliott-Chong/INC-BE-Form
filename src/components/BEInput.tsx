b1df27a0eaf8214deecd82176c8b9edb8dcb3bfa
Elliott Chong <elliottchong16@gmail.com>
2023-10-22 14:15:27 +0800

    set up tailwind and shadcn

---
+++ src/components/BEInput.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+type Props = {};
+
+const BEInput = (props: Props) => {
+  return <Button>hello world</Button>;
+};
+
+export default BEInput;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { type AppType } from "next/app";
-
+import { Inter } from "next/font/google";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+const inter = Inter({ subsets: ["latin"] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div className={`${inter.className} min-h-screen`}>
+        <Component {...pageProps} />;
+      </div>
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
