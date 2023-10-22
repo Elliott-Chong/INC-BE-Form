91709aac0af9aed722d952f6e0e44d96f7df9bb6
Zolastic <honhattienlul@gmail.com>
2023-10-22 22:12:54 +0800

    Added sidebar into the page layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,29 @@
 import React, { PropsWithChildren, useState } from "react";
+import Sidebar from "../components/Sidebar";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 const PageLayout = (props: PropsWithChildren) => {
-  return <main className="overflow-none flex h-screen justify-center"></main>;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <main className="flex h-full w-full">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="m-4">
+        <button
+          type="button"
+          className="-m-2.5 p-2.5 text-gray-700"
+          onClick={() => setSidebarOpen(true)}
+        >
+          <span className="sr-only">Open sidebar</span>
+          <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+
+      {/* Separator */}
+      <div className="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
+      {props.children}
+    </main>
+  );
 };
 
 export default PageLayout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 import { Fragment, useState } from "react";
-import {Menu, Transition } from "@headlessui/react";
-import Sidebar from "../components/Sidebar";
-import {
-  Bars3Icon,
-  BellIcon,
-} from "@heroicons/react/24/outline";
+import { Menu, Transition } from "@headlessui/react";
+import { BellIcon } from "@heroicons/react/24/outline";
 import {
   ChevronDownIcon,
   MagnifyingGlassIcon,
@@ -18,8 +14,6 @@ const userNavigation = [
 ];
 
 export default function Example() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   return (
     <>
       {/*
@@ -30,25 +24,9 @@ export default function Example() {
         <body class="h-full">
         ```
       */}
-      <div>
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="h-full w-full">
         <div>
           <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-            <button
-              type="button"
-              className="-m-2.5 p-2.5 text-gray-700"
-              onClick={() => setSidebarOpen(true)}
-            >
-              <span className="sr-only">Open sidebar</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-
-            {/* Separator */}
-            <div
-              className="h-6 w-px bg-gray-900/10 lg:hidden"
-              aria-hidden="true"
-            />
-
             <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
               <form className="relative flex flex-1" action="#" method="GET">
                 <label htmlFor="search-field" className="sr-only">
