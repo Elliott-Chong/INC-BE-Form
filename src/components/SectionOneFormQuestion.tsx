54a79f16e4e9a82c7cce174c773b1ee330c6768d
Zolastic <honhattienlul@gmail.com>
2023-10-22 21:43:27 +0800

    feat: Created files for BEForm Section One

---
+++ src/components/SectionOneFormQuestion.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SectionOneFormQuestion = (props: Props) => {
+  return <div>SectionOneFormQuestion</div>;
+};
+
+export default SectionOneFormQuestion;
---
+++ src/components/layout.tsx
@@ -0,0 +1,7 @@
+import React, { PropsWithChildren, useState } from "react";
+
+const PageLayout = (props: PropsWithChildren) => {
+  return <main className="overflow-none flex h-screen justify-center"></main>;
+};
+
+export default PageLayout;
