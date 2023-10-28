2d8d75974cd2cd3c798d42801b0b3f5591a80b28
Zolastic <honhattienlul@gmail.com>
2023-10-28 18:04:33 +0800

    feat: made text inputs for BEForm Part 2 vertical

--- src/components/BEForm/PartTwoFormQuestion.tsx
+++ src/components/BEForm/PartTwoFormQuestion.tsx
@@ -102,27 +102,29 @@ const PartTwoFormQuestion = (props: Props) => {
         </div>
 
         <div className="mt-2 flex space-x-1">
-          <div className="w-full">
-            <Label>Strengths</Label>
-            <textarea
-              id="about"
-              name="about"
-              rows={3}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              value={value.strengths}
-              onChange={handleStrengthsChange}
-            />
-          </div>
-          <div className="w-full">
-            <Label>Areas for improvement</Label>
-            <textarea
-              id="about"
-              name="about"
-              rows={3}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              value={value.areasForImprovement}
-              onChange={handleAreasForImprovementChange}
-            />
+          <div className="flex w-full flex-col">
+            <div className="w-full">
+              <Label>Strengths</Label>
+              <textarea
+                id="about"
+                name="about"
+                rows={3}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={value.strengths}
+                onChange={handleStrengthsChange}
+              />
+            </div>
+            <div className="w-full">
+              <Label>Areas for improvement</Label>
+              <textarea
+                id="about"
+                name="about"
+                rows={3}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={value.areasForImprovement}
+                onChange={handleAreasForImprovementChange}
+              />
+            </div>
           </div>
           <div className="grow"></div>
           <div className="mt-7">
