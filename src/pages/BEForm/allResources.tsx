db18ac85813403d6deb51daf28b10cb0ccf072f6
Zolastic <honhattienlul@gmail.com>
2023-11-02 13:54:15 +0800

    feat: updated the links of the resources

--- src/pages/BEForm/allResources.tsx
+++ src/pages/BEForm/allResources.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React from "react";
 
 type Props = {};
 
 const Part1 = () => {
   return (
-    <div className="p-4 m-2">
-      <h2 className="text-2xl font-bold p-2">Part 1</h2>
+    <div className="m-2 p-4">
+      <h2 className="p-2 text-2xl font-bold">PART 1: Organisational Profile</h2>
       <table className="w-full">
         <tbody>
           <tr>
-            <th className="text-left p-2 border">Section</th>
-            <th className="text-left p-2 border">Resource</th>
+            <th className="w-1/2 border p-2 text-left">Section</th>
+            <th className="w-1/2 border p-2 text-left">Resource</th>
           </tr>
           <tr>
-            <td className="p-2 border">Section 1</td>
-            <td className="p-2 border">
-              <a href="https://www.google.com" className="text-blue-300 visited:text-purple-600">Google</a>
+            <td className="border p-2">Section 1</td>
+            <td className="border p-2">
+              <a
+                href="https://docs.google.com/presentation/d/1qjyVG1uPRYfRmnvL50Zbs13yXqKuWd2Dm4Gtn20NBO8/edit?usp=sharing"
+                className="text-blue-300 visited:text-purple-600"
+              >
+                Presentation
+              </a>
             </td>
           </tr>
         </tbody>
@@ -26,18 +31,25 @@ const Part1 = () => {
 
 const Part2 = () => {
   return (
-    <div className="p-4 m-2">
-      <h2 className="text-2xl font-bold p-2">Part 2</h2>
+    <div className="m-2 p-4">
+      <h2 className="p-2 text-2xl font-bold">
+        PART 2: Organisational System Assessment
+      </h2>
       <table className="w-full">
         <tbody>
           <tr>
-            <th className="text-left p-2 border">Section</th>
-            <th className="text-left p-2 border">Resource</th>
+            <th className="w-1/2 border p-2 text-left">Section</th>
+            <th className="w-1/2 border p-2 text-left">Resource</th>
           </tr>
           <tr>
-            <td className="p-2 border">Section 1</td>
-            <td className="p-2 border">
-              <a href="https://www.google.com" className="text-blue-300 visited:text-purple-600">Google</a>
+            <td className="border p-2">Section 1</td>
+            <td className="border p-2">
+              <a
+                href="https://docs.google.com/document/d/1XULffhUj-zpFtRFF9UJ2h5vzY1_QH0kj6coAu67ihbs/edit?usp=sharing"
+                className="text-blue-300 visited:text-purple-600"
+              >
+                Ideation
+              </a>
             </td>
           </tr>
         </tbody>
@@ -48,18 +60,23 @@ const Part2 = () => {
 
 const Part3 = () => {
   return (
-    <div className="p-4 m-2">
-      <h2 className="text-2xl font-bold p-2">Part 3</h2>
+    <div className="m-2 p-4">
+      <h2 className="p-2 text-2xl font-bold">Part 3: Organisational Results</h2>
       <table className="w-full">
         <tbody>
           <tr>
-            <th className="text-left p-2 border">Section</th>
-            <th className="text-left p-2 border">Resource</th>
+            <th className="w-1/2 border p-2 text-left">Section</th>
+            <th className="w-1/2 border p-2 text-left">Resource</th>
           </tr>
           <tr>
-            <td className="p-2 border">Section 7</td>
-            <td className="p-2 border">
-              <a href="https://www.google.com" className="text-blue-300 visited:text-purple-600">Google</a>
+            <td className="border p-2">Section 7</td>
+            <td className="border p-2">
+              <a
+                href="https://media.tenor.com/jUMex_rdqPwAAAAM/among-us-twerk.gif"
+                className="text-blue-300 visited:text-purple-600"
+              >
+                Mascot
+              </a>
             </td>
           </tr>
         </tbody>
@@ -71,7 +88,7 @@ const Part3 = () => {
 const AllResources = (props: Props) => {
   return (
     <div className="flex flex-col justify-center">
-      <div className="p-4 m-2">
+      <div className="m-2 p-4">
         <h1 className="text-3xl font-bold">All Resources</h1>
       </div>
       <Part1 />
