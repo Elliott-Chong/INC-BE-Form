98903c31ad4812517da8953ac5f3b65153f4793c
jianmin22 <jianmin.22@ichat.sp.edu.sg>
2023-11-03 15:51:42 +0800

    changed resources

--- src/pages/BEForm/allResources.tsx
+++ src/pages/BEForm/allResources.tsx
@@ -2,98 +2,115 @@ import React from "react";
 
 type Props = {};
 
-const Part1 = () => {
+const Part = ({ partName, sections }: any) => {
   return (
     <div className="m-2 p-4">
-      <h2 className="p-2 text-2xl font-bold">PART 1: Organisational Profile</h2>
-      <table className="w-full">
-        <tbody>
-          <tr>
-            <th className="w-1/2 border p-2 text-left">Section</th>
-            <th className="w-1/2 border p-2 text-left">Resource</th>
-          </tr>
-          <tr>
-            <td className="border p-2">Section 1</td>
-            <td className="border p-2">
-              <a
-                href="https://docs.google.com/presentation/d/1qjyVG1uPRYfRmnvL50Zbs13yXqKuWd2Dm4Gtn20NBO8/edit?usp=sharing"
-                className="text-blue-300 visited:text-purple-600"
-              >
-                Presentation
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-const Part2 = () => {
-  return (
-    <div className="m-2 p-4">
-      <h2 className="p-2 text-2xl font-bold">
-        PART 2: Organisational System Assessment
-      </h2>
-      <table className="w-full">
-        <tbody>
-          <tr>
-            <th className="w-1/2 border p-2 text-left">Section</th>
-            <th className="w-1/2 border p-2 text-left">Resource</th>
-          </tr>
-          <tr>
-            <td className="border p-2">Section 1</td>
-            <td className="border p-2">
-              <a
-                href="https://docs.google.com/document/d/1XULffhUj-zpFtRFF9UJ2h5vzY1_QH0kj6coAu67ihbs/edit?usp=sharing"
-                className="text-blue-300 visited:text-purple-600"
-              >
-                Ideation
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-const Part3 = () => {
-  return (
-    <div className="m-2 p-4">
-      <h2 className="p-2 text-2xl font-bold">Part 3: Organisational Results</h2>
-      <table className="w-full">
-        <tbody>
-          <tr>
-            <th className="w-1/2 border p-2 text-left">Section</th>
-            <th className="w-1/2 border p-2 text-left">Resource</th>
-          </tr>
-          <tr>
-            <td className="border p-2">Section 7</td>
-            <td className="border p-2">
-              <a
-                href="https://media.tenor.com/jUMex_rdqPwAAAAM/among-us-twerk.gif"
-                className="text-blue-300 visited:text-purple-600"
-              >
-                Mascot
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <h2 className="p-2 text-2xl font-bold">{partName}</h2>
+      {sections.map((section: any, sectionIndex: number) => (
+        <table key={sectionIndex} className="w-full">
+          <tbody>
+            <tr>
+              <th className="w-1/3 border p-2 text-left">Section</th>
+              <th className="w-1/3 border p-2 text-left">Question</th>
+              <th className="w-1/3 border p-2 text-left">Resource</th>
+            </tr>
+            {section.questions.map((question: any, questionIndex: number) => (
+              <tr key={questionIndex}>
+                {questionIndex === 0 ? (
+                  <td className="border p-2" rowSpan={section.questions.length}>{section.sectionName}</td>
+                ) : (
+                  null
+                )}
+                <td className="border p-2">{question.questionText}</td>
+                <td className="border p-2">
+                  <a
+                    href={question.resourceLink}
+                    className="text-blue-300 visited:text-purple-600"
+                  >
+                    {question.resourceName}
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ))}
     </div>
   );
 };
 
 const AllResources = (props: Props) => {
+  const parts = [
+    {
+      partName: "PART 1: Organisational Profile",
+      sections: [
+        {
+          sectionName: "1",
+          questions: [
+            {
+              questionText: "Question 1",
+              resourceLink: "https://splms.polite.edu.sg/d2l/home/6667",
+              resourceName: "Resource 1",
+            },
+            {
+              questionText: "Question 2",
+              resourceLink: "https://splms.polite.edu.sg/d2l/home/6667",
+              resourceName: "Resource 2",
+            }
+          ],
+        },
+      ],
+    },
+    {
+      partName: "PART 2: Organisational System Assessment",
+      sections: [
+        {
+          sectionName: "2",
+          questions: [
+            {
+              questionText: "Question 1",
+              resourceLink: "https://splms.polite.edu.sg/d2l/home/6667",
+              resourceName: "Resource 1",
+            },
+            {
+              questionText: "Question 2",
+              resourceLink: "https://splms.polite.edu.sg/d2l/home/6667",
+              resourceName: "Resource 2",
+            }
+          ],
+        },
+      ],
+    },
+    {
+      partName: "Part 3: Organisational Results",
+      sections: [
+        {
+          sectionName: "3",
+          questions: [
+            {
+              questionText: "Question 1",
+              resourceLink: "https://splms.polite.edu.sg/d2l/home/6667",
+              resourceName: "Resource 1",
+            },
+            {
+              questionText: "Question 2",
+              resourceLink: "https://splms.polite.edu.sg/d2l/home/6667",
+              resourceName: "Resource 2",
+            }
+          ],
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="flex flex-col justify-center">
       <div className="m-2 p-4">
         <h1 className="text-3xl font-bold">All Resources</h1>
       </div>
-      <Part1 />
-      <Part2 />
-      <Part3 />
+      {parts.map((part, index) => (
+        <Part key={index} partName={part.partName} sections={part.sections} />
+      ))}
     </div>
   );
 };
